2da90f2cc35e0ce278731d745aafe566d3d5210a
pallav100 <100pallav@gmail.com>
2023-12-02 20:50:08 -0800

    add APIs

--- routes/orderPlacement.js
+++ routes/orderPlacement.js
@@ -10,10 +10,55 @@ router.post('/', function(req, res) {
   res.send({ title: 'placed' });
 });
 
-router.get('/test', function(req, res) {
+router.post('/api/v2/orderSide/market/currencyPair', function(req, res) {
   console.log(global.io);
 
-  res.render('test');
+  const body = req.body;
+  console.log(body);
+  res.send({
+    "id": "1234",
+    "market": "FET/USD",
+    "datetime": "2023-12-31 14:43:15.796000",
+    "type": "0",
+    "price": "22.45",
+    "amount": "58.06000000",
+    "client_order_id": "123456789"
+  });
+});
+
+router.get('/0/private/AddOrder', function(req, res) {
+  console.log(global.io);
+  const body = req.body;
+  console.log(body);
+  res.send({
+    "error": [],
+    "result": {
+        "descr": {
+            "order": "sell 22.450000 FETUSD @ limit 58.14"
+        },
+        "txid": [
+            "OU22CG-KLAF2-FWUDD7"
+        ]
+    }
+  });
+});
+
+// TODO
+router.post('/v2/auth/w/order/submit', function(req, res) {
+  console.log(global.io);
+  const body = req.body;
+  console.log(body);
+  res.send({
+    "error": [],
+    "result": {
+        "descr": {
+            "order": "sell 22.450000 FETUSD @ limit 58.14"
+        },
+        "txid": [
+            "OU22CG-KLAF2-FWUDD7"
+        ]
+    }
+  });
 });
 
 
--- routes/orderStatus.js
+++ routes/orderStatus.js
@@ -5,5 +5,72 @@ var router = express.Router();
 router.post('/', function(req, res) {
   res.send({'id': 1010, 'status':'fulfilled'});
 });
+router.post('/api/v2/order_status/', function(req, res) {
+  res.send({
+    "id": 1234,
+    "datetime": "2023-01-31 14:45:15",
+    "type": "0",
+    "status": "Finished",
+    "market": "FET/USD",
+    "transactions": [
+        {
+            "tid": "1458532827766784",
+            "price": "58.06",
+            "{FET}": "22.45",
+            "{USD}": "1",
+            "fee": "0.00",
+            "datetime": "2023-01-31 14:45:15",
+            "type": 0
+        }
+    ],
+    "amount_remaining": "0.00",
+    "client_order_id": "0.50000000"
+  });
+});
+// TODO
+router.post('/0/private/QueryOrders', function(req, res) {
+  res.send({
+    "error": [],
+    "result": {
+        "OU22CG-KLAF2-FWUDD7": {
+            "refid": "None",
+            "userref": 0,
+            "status": "closed",
+            "reason": null,
+            "opentm": 1688665496.7808,
+            "closetm": 1688665499.1922,
+            "starttm": 0,
+            "expiretm": 0,
+            "descr": {
+                "pair": "FETUSD",
+                "type": "sell",
+                "ordertype": "market",
+                "price": "58.14",
+                "price2": "0",
+                "leverage": "none",
+                "order": "buy 22.45000 FETUSD @ limit 58.14",
+                "close": ""
+            },
+            "vol": "22.4500000",
+            "vol_exec": "22.4500000",
+            "cost": "1305.243",
+            "fee": "0.0",
+            "price": "58.14",
+            "stopprice": "0.00000",
+            "limitprice": "0.00000",
+            "misc": "",
+            "oflags": "fciq",
+            "trigger": "index",
+            "trades": [
+                "TZX2WP-XSEOP-FP7WYR"
+            ]
+        }
+    }
+  });
+});
+// TODO
+router.post(`/auth/r/order/` + `:currencyPair:` + `:orderId` + `/trades`, function(req, res) {
+  res.send({'id': 1010, 'status':'fulfilled'});
+});
 
 module.exports = router;
