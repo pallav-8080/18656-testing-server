61402a857c67b904f9339937b5fe20f567762823
pallav100 <100pallav@gmail.com>
2023-12-06 23:32:39 -0800

    port 80

--- socket.js
+++ socket.js
@@ -1,28 +1,9 @@
+var socketio = require('socket.io')
 var data = require('./liveFeed.js')
-var WsServer= require('ws').Server;
 
 function spinWebSocketserver(server){
 
-const sockserver = new WsServer({ server: server })
-sockserver.on('connection', ws => {
-  console.log("ll")
-  var a = {
-    'alal': 1,
-    "mmmm": 2,
-    "pol": "mmm"
-  };
-  ws.send(JSON.stringify(a));
-  ws.on('close', () => console.log('Client has disconnected!'))
-  // ws.on('message', data => {
-  //   // sockserver.clients.forEach(client => {
-  //     console.log(data.)
-  //     // client.send(`hello`)
-  // })
-  // // })
-  ws.onerror = function () {
-    console.log('websocket error')
-  }
- })
+    
     
 }
 
